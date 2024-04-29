f89a412b1ad2f0122340c6690d44e46f2f16d11a
nobregas <gabrielnobregasantos2@gmail.com>
2024-04-29 14:36:10 -0300

    feat: implementacao da config inicial do OpenApiConfig / swagger

---
+++ src/main/java/br/com/currencyconverterapi/apicurrencyconverter/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package br.com.currencyconverterapi.apicurrencyconverter.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Currency Converter Api"
+        )
+)
+public class OpenApiConfig {
+}
