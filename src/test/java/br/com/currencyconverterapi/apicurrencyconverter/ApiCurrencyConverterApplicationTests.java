a539e1e112321861270b2c4174a4c772fe28bfd2
nobregas <gabrielnobregasantos2@gmail.com>
2024-04-29 13:19:05 -0300

    feat: começando projeto

---
+++ src/test/java/br/com/currencyconverterapi/apicurrencyconverter/ApiCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.currencyconverterapi.apicurrencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
