a92257c7b56b8b73c0421dce3a925b320301c82e
nobregas <gabrielnobregasantos2@gmail.com>
2024-05-03 13:57:40 -0300

    feat: created log config

--- src/main/java/br/com/currencyconverterapi/apicurrencyconverter/ApiCurrencyConverterApplication.java
+++ src/main/java/br/com/currencyconverterapi/apicurrencyconverter/ApiCurrencyConverterApplication.java
@@ -1,13 +1,17 @@
 package br.com.currencyconverterapi.apicurrencyconverter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ApiCurrencyConverterApplication {
 
     public static void main(String[] args) {
+        log.info("application start - BEGIN");
         SpringApplication.run(ApiCurrencyConverterApplication.class, args);
+        log.info("application start - END");
     }
 
 }
