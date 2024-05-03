9339ba2b644a2549bf9d59e5b50317d2b40e80d8
nobregas <gabrielnobregasantos2@gmail.com>
2024-05-03 13:54:08 -0300

    feat: swagger

--- src/main/java/br/com/currencyconverterapi/apicurrencyconverter/application/config/OpenApiConfig.java
+++ src/main/java/br/com/currencyconverterapi/apicurrencyconverter/application/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package br.com.currencyconverterapi.apicurrencyconverter.config;
+package br.com.currencyconverterapi.apicurrencyconverter.application.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
